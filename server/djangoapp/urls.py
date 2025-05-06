f67c0061546970dd40c82d3e6ce0af068c2e17f6
gnl2024 <gnlauture@gmail.com>
2025-05-06 16:11:55 -0400

    logout route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path('logout', views.logout_request, name='logout'),
     
     
 

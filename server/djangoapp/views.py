0f9f4e1215ecb265c2cf9650b93c612302c5b981
gnl2024 <gnlauture@gmail.com>
2025-05-26 10:36:35 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,26 +1,17 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-
-# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-
-# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-
-# from django.contrib import messages
-# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
-
-# import requests
 from .models import CarMake, CarModel
+from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -48,9 +39,6 @@ def logout_request(request):
     logout(request)  # Terminate user session
     data = {"userName": ""}  # Return empty username
     return JsonResponse(data)
-
-
-# ...
 
 
 def get_cars(request):
